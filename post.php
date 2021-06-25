58e01c0f8766754a4bb5928b171bd7f9ab7ea7eb
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-06-25 15:24:45 +0800

    begin

---
+++ post.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+<?php include "header.php" ?>
+ 
+<!-- Menampikan kolom sebelah kiri -->
+
+<div class="row">
+   <div class="leftcolumn">
+      <div class="card">
+      <div class="card">
+   <h2>TITLE HEADING</h2>
+   <h5>Title description, Dec 7, 2017</h5>
+   <div class="fakeimg" style="height:200px;">Image</div>
+   <p>
+   
+   </p>
+</div>
+>
+      </div>
+   </div>
+
+   <?php include "footer.php" ?>
+</div>
+ 
+</body>
+</html>
