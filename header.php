db441da86a2214ce3f4bc1d7a9e016c7f29d985b
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-06-25 15:25:07 +0800

    bagin

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+<div class="header">
+ <h1>DENPASAR NEWS</h1>
+ <p>informasi seputar Denpasar</p>
+</div>
+ 
+<!-- Menampikan kolom sebelah kiri -->
+<div class="row">
+ <div class="leftcolumn">
+ </div>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+<?php include "header.php" ?>
+ 
+<!-- Menampikan kolom sebelah kiri -->
+
+<div class="row">
+ <div class="leftcolumn">
+   <div class="card">
+     <a href="post.php"><h2>TITLE HEADING</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height: 200px;">
+     
+     </div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>TITLE HEADING</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>TITLE HEADING</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+ 
+ </div>
+
+
+<?php include "footer.php" ?>
+ 
+</body>
+</html>
